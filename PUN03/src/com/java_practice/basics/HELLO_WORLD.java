d406210ce2efb0645b87208c47c85cf0b4d1a729
PrathameshTondilkar <prathameshtondilkar001@gmail.com>
2024-08-24 19:02:33 +0530

    First Program(Hello World)

---
+++ PUN03/src/com/java_practice/basics/HELLO_WORLD.java
@@ -0,0 +1,10 @@
+package com.java_practice.basics;
+
+public class HELLO_WORLD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Friends ");
+	}
+
+}
